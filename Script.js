b84e32da05a6fc7a7b687ab3c47f60870fdd76da
Sultan <128064384+Sultan-08@users.noreply.github.com>
2023-11-24 21:06:23 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+let button = document.querySelector("button");
+let sticker = document.querySelector(".sticker");
+let button2 = document.querySelector("#btn2");
+let button3 = document.querySelector("#btn3");
+// const parent = document.querySelector(".button");
+// parent.addEventListener("click", function(e) {
+//     const child = e.target.matches(".child, .child *");
+//     sticker.style.left = "50rem";
+// });
+
+document.addEventListener("click", ()=> {
+        button2.style.transition = "10s";
+        button2.style.left = "38em";
+        button3.style.transition = "10s";
+        button3.style.left = "30em";
+    });
+
+    function openForm() {
+        document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+    }
